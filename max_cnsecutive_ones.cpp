db92bea23e1b81f0cfb0fcffc18cef14c8097099
ibtee12 <2203004@student.ruet.ac.bd>
2026-03-10 19:11:24 +0600

    max_cons_1s pushed

---
+++ max_cnsecutive_ones.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+void max_Ones(vector<int> arr){
+   int cnt=0,max=0;
+
+for(int i=0;i<arr.size();i++){
+    if(arr[i]==1){
+      cnt++;
+    } 
+    if(arr[i]==0 || i==arr.size()-1){ //this condition is the main part
+        if(cnt>max) max=cnt;
+
+        cnt=0;
+
+    } 
+}
+cout<<max;
+}
+int main(){
+    
+    vector<int> arr1={0,0,1,1,0,0,1,1,0,1,1,1,0,1,1,1,1,1,0,1};
+    max_Ones(arr1);
+    return 0;
+}
